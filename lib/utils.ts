039b1e7e31752f020a88a99160312be0911e7275
Matej Bittner <mates-bittner@seznam.cz>
2024-09-24 15:00:24 +0200

    images

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,43 +3,43 @@ export function getWeatherImage(weatherCode: number) {
 
   switch (true) {
     case weatherCode === 800:
-      imageName = "Clear";
+      imageName = "clear";
       break;
 
     case weatherCode === 801 || weatherCode === 802:
-      imageName = "Clouds-801-802";
+      imageName = "clouds-801-802";
       break;
 
     case weatherCode === 803:
-      imageName = "Clouds-803";
+      imageName = "clouds-803";
       break;
 
     case weatherCode === 804:
-      imageName = "Clouds-804";
+      imageName = "clouds-804";
       break;
 
     case weatherCode === 500 || weatherCode === 501:
-      imageName = "Rain-500-501";
+      imageName = "rain-500-501";
       break;
 
     case weatherCode >= 502 && weatherCode <= 531:
-      imageName = "Rain-502-531";
+      imageName = "rain-502-531";
       break;
 
     case weatherCode >= 202 && weatherCode <= 212:
-      imageName = "Thunderstorm-202-212";
+      imageName = "thunderstorm-202-212";
       break;
 
     case weatherCode === 200 || weatherCode === 201:
-      imageName = "Thunderstorm-200-201";
+      imageName = "thunderstorm-200-201";
       break;
 
     case weatherCode >= 600 && weatherCode <= 622:
-      imageName = "Snow";
+      imageName = "snow";
       break;
 
     default:
-      imageName = "Default";
+      imageName = "default";
       break;
   }
 
